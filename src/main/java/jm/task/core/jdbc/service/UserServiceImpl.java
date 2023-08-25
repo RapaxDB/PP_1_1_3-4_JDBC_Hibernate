d23a1f3b9e7f8c5a81dbddc9ce173d9e07d89e45
Дмитрий Белоусов <72445496+RapaxDB@users.noreply.github.com>
2023-08-25 20:17:53 +0500

    Update UserServiceImpl

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,35 +1,36 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.*;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDao daoJDBC = new UserDaoHibernateImpl();
+    private final UserDao dao = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        daoJDBC.createUsersTable();
+        dao.createUsersTable();
     }
 
 
     public void dropUsersTable() {
-        daoJDBC.dropUsersTable();
+        dao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        daoJDBC.saveUser(name, lastName, age);
+        dao.saveUser(name, lastName, age);
         System.out.println("User с именем – " + name + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
-        daoJDBC.removeUserById(id);
+        dao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return daoJDBC.getAllUsers();
+        return dao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        daoJDBC.cleanUsersTable();
+        dao.cleanUsersTable();
     }
 }
